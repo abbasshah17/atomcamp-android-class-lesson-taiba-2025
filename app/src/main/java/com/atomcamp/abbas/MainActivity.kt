9921058d9ca117dea25f4f28ea8256de98fad3cc
Abbas Anwer Ali <abbasshah17@gmail.com>
2025-03-06 16:09:25 +0500

    Adding examples for a stateful Composable.

--- app/src/main/java/com/atomcamp/abbas/MainActivity.kt
+++ app/src/main/java/com/atomcamp/abbas/MainActivity.kt
@@ -5,39 +5,20 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.atomcamp.abbas.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,6 +34,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Log.i("MainActivity", "Scaffold is being rendered")
+
                     Greeting(
                         name = "Abbas",
                         modifier = Modifier.padding(innerPadding)
@@ -96,23 +79,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
-    var counter: MutableState<Int> = remember {
+    Log.i("MainActivity", "Greeting is being rendered")
+
+    var counter: Int by remember {
         mutableStateOf(0)
     }
 
-    Box(
+    Column (
         modifier = modifier
-            .fillMaxSize()
-            .background(color = Color.Red),
-        contentAlignment = Alignment.Center,
+            .fillMaxSize(),
     ) {
-        Image(
-            painter = painterResource(R.drawable.compose_image),
-            contentDescription = "This is an image of Compose",
-        )
+        Button(onClick = {
+            counter = counter + 1
+        }) {
+            Text("Click me")
+        }
 
         Text(
-            text = "Hello this is your Trainer $name!",
+            text = "${counter}",
             modifier = Modifier
                 .clickable {
                     Log.i("Greeting", "The Text Composable has been clicked.")
