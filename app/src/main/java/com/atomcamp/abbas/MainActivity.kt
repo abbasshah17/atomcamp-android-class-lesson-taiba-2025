f2a8bb4cbab08af4cfdba4d817730ef407534777
Abbas Anwer Ali <abbasshah17@gmail.com>
2025-03-06 15:30:50 +0500

    Adding examples for Composables:
    1. Box,
    2. Image.

--- app/src/main/java/com/atomcamp/abbas/MainActivity.kt
+++ app/src/main/java/com/atomcamp/abbas/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -20,9 +21,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -89,18 +96,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
-    Column(
+    var counter: MutableState<Int> = remember {
+        mutableStateOf(0)
+    }
+
+    Box(
         modifier = modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.End,
+            .fillMaxSize()
+            .background(color = Color.Red),
+        contentAlignment = Alignment.Center,
     ) {
-        Text(
-            text = "Hello this is your Trainer $name!",
-            modifier = Modifier
-                .clickable {
-                    Log.i("Greeting", "The Text Composable has been clicked.")
-                },
+        Image(
+            painter = painterResource(R.drawable.compose_image),
+            contentDescription = "This is an image of Compose",
         )
 
         Text(
@@ -110,25 +118,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     Log.i("Greeting", "The Text Composable has been clicked.")
                 },
         )
-
-        Text(
-            text = "Hello this is your Trainer $name!",
-            modifier = Modifier
-                .clickable {
-                    Log.i("Greeting", "The Text Composable has been clicked.")
-                },
-        )
-
-        Button(onClick = {
-
-        }) {
-            Text(
-                text = "Hello this is your Trainer $name!",
-                modifier = Modifier
-                    .background(color = Color.Blue)
-                    .fillMaxWidth(),
-            )
-        }
     }
 }
 
