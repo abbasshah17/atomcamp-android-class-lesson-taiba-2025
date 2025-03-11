8054cef267c64eaf5dfe9fe8eaed72c0dffff30d
Abbas Anwer Ali <abbasshah17@gmail.com>
2025-03-11 13:52:01 +0500

    Explored LinearLayout and TextView and Button Views in XML Layout.

--- app/src/main/java/com/atomcamp/abbas/MainActivity.kt
+++ app/src/main/java/com/atomcamp/abbas/MainActivity.kt
@@ -3,7 +3,6 @@ package com.atomcamp.abbas
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -29,31 +28,9 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
 
-        setContent {
-            AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Log.i("MainActivity", "Scaffold is being rendered")
-
-                    ListScreen(
-                        modifier = Modifier
-                            .padding(innerPadding),
-                        items = listOf(
-                            ProfileExtended.Example1,
-                            Profile.Example1,
-                            Profile.Example2,
-                            Profile.Example3,
-                            Profile.Example4,
-                            Profile.Example5,
-                            Profile.Example6,
-                            Profile.Example7,
-                            Profile.Example8,
-                        )
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 
     override fun onStart() {
